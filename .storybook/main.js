a8257e171329dcc38818a4a42e334e5539956ee5
NeOMakinG <valentin.szczupak@prestashop.com>
2021-02-26 16:44:39 +0100

    Add variables story

--- .storybook/main.js
+++ .storybook/main.js
@@ -11,6 +11,11 @@ module.exports = {
   webpackFinal: (config) => {
     config.resolve.alias['core-js/modules'] = '@storybook/core/node_modules/core-js/modules';
     config.module.rules[3].use = 'html-loader?minimize=false';
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+      include: path.resolve(__dirname, '../'),
+    });
 
     return config;
   },
